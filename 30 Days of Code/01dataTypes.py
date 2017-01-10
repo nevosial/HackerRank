6bcf51893785f9404c8c244e1c394f21e201203a
nevosial <nevosial@gmail.com>
2017-01-10 11:35:55 +0530

    Update 01dataTypes.py

--- 30 Days of Code/01dataTypes.py
+++ 30 Days of Code/01dataTypes.py
@@ -1,13 +1,16 @@
 # Declare second integer, double, and String variables.
-
+i = 4
+d = 4.0
+s = 'HackerRank '
 
 # Read and save an integer, double, and String to your variables.
 j = raw_input()
 k = raw_input()
-#l = "is the best place to learn and practice coding!"
 l = raw_input()
+
 # Print the sum of both integer variables on a new line.
 print i + int(j)
+
 # Print the sum of the double variables on a new line.
 print d + float(k)
 
