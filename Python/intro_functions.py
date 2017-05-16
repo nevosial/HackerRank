0f35c94ee30062ddeec77693d1d3ee72f606bf73
Neville Mascarenhas <nevosial@gmail.com>
2017-05-16 11:56:26 +0530

    Intro_functions
    Leap year identifier using functions

---
+++ Python/intro_functions.py
@@ -0,0 +1,18 @@
+#The year can be evenly divided by 4;
+#If the year can be evenly divided by 100, it is NOT a leap year, unless;
+#The year is also evenly divisible by 400. Then it is a leap year.
+
+
+def is_leap(year):
+    leap = False
+    
+    if year % 4 == 0:
+        if year % 100 == 0 and year % 400 == 0:
+            return True
+        elif year % 100 == 0 and year % 400 != 0:
+            return False
+        else:
+            return True
+    
+    
+    return leap
